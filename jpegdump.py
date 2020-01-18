31aaa6a227677074fcc214e25c84cbafcf080a8d
Victor Derks <vbaderks@gmail.com>
2020-01-18 18:11:18 +0100

    Add __dump_start_of_scan

--- jpegdump.py
+++ jpegdump.py
@@ -25,7 +25,8 @@ class JpegReader:
         self.jpegls_stream = False
         self.dump_functions = {JpegMarker.START_OF_IMAGE: self.__dump_start_of_image_marker,
                                JpegMarker.END_OF_IMAGE: self.__dump_end_of_image,
-                               JpegMarker.START_OF_FRAME_JPEGLS: self.__dump_start_of_frame_jpegls}
+                               JpegMarker.START_OF_FRAME_JPEGLS: self.__dump_start_of_frame_jpegls,
+                               JpegMarker.START_OF_SCAN: self.__dump_start_of_scan}
 
     def dump(self) -> None:
         byte = self.__read_bytes()
@@ -66,7 +67,7 @@ class JpegReader:
         print(f"{self.__get_start_offset:8} Marker 0xFFD9. EOI (End Of Image), defined in ITU T.81/IEC 10918-1")
 
     def __dump_start_of_frame_jpegls(self) -> None:
-        print(f"{self.__get_start_offset :8} Marker 0xFFF7: SOF_55 (Start Of Frame JPEG-LS),"
+        print(f"{self.__get_start_offset:8} Marker 0xFFF7: SOF_55 (Start Of Frame JPEG-LS),"
               f" defined in ITU T.87/IEC 14495-1 JPEG LS")
         print(f"{self.__position:8}  Size = {self.__read_uint16_big_endian()}")
         print(f"{self.__position:8}  Sample precision (P) = {self.__read_byte()}")
@@ -82,9 +83,28 @@ class JpegReader:
             print(f"{position:8}   H and V sampling factor (Hi + Vi) = {sampling_factor}"
                   f" ({sampling_factor >> 4} + {sampling_factor & 0xF})") 
             print(f"{self.__position:8}   Quantization table (Tqi) [reserved, should be 0] = {self.__read_byte()}")
+        jpegls_stream = True
+
+    def __dump_start_of_scan(self) -> None:
+        print(f"{self.__get_start_offset:8} Marker 0xFFDA: SOS (Start Of Scan), defined in ITU T.81/IEC 10918-1")
+        print(f"{self.__position:8}  Size = {self.__read_uint16_big_endian()}")
+        component_count: int = self.__read_byte()
+        print(f"{self.__position:8}  Component Count = {component_count}")
+        for component in range(component_count):
+            print(f"{self.__position:8}   Component identifier (Ci) = {1}", self.__read_byte())
+            mapping_table_selector: int = self.__read_byte()
+            print(f"{self.__position:8}   Mapping table selector = {mapping_table_selector}")
+        print(f"{self.__position:8}  Near lossless (NEAR parameter) = {self.__read_byte()}")
+        interleave_mode: int = self.__read_byte()
+        print(f"{self.__position:8}  Interleave mode (ILV parameter) = {interleave_mode}"
+              f" ({self.__get_interleave_modename(interleave_mode)})")
+        print(f"{self.__position:8}  Point Transform = {self.__read_byte()}")
 
     def __dump_unknown_marker(self, marker_code: int) -> None:
         print(f"{self.__get_start_offset :8} Marker 0xFF{marker_code:02X}")
+
+    def __get_interleave_modename(self, interleave_mode: int) -> str:
+        pass
 
     @property
     def __position(self) -> int:
