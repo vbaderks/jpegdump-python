124dc6d1fd55c3d45408b84bf3ccd2057d1181b7
Victor Derks <vbaderks@gmail.com>
2020-01-18 13:53:20 +0100

    Add __dump_start_of_frame_jpegls

--- jpegdump.py
+++ jpegdump.py
@@ -23,19 +23,26 @@ class JpegReader:
     def __init__(self, file: BinaryIO) -> None:
         self.file = file
         self.jpegls_stream = False
-        self.dump_functions = {JpegMarker.START_OF_IMAGE: self.__dump_start_of_image_marker}
+        self.dump_functions = {JpegMarker.START_OF_IMAGE: self.__dump_start_of_image_marker,
+                               JpegMarker.START_OF_FRAME_JPEGLS: self.__dump_start_of_frame_jpegls}
 
     def dump(self) -> None:
-        byte = self.__read_byte()
+        byte = self.__read_bytes()
         while byte:
             if byte[0] == 0xFF:
-                byte = self.__read_byte()
+                byte = self.__read_bytes()
                 if self.__is_marker_code(byte[0]):
                     self.__dump_marker_code(byte[0])
-            byte = self.__read_byte()
+            byte = self.__read_bytes()
 
-    def __read_byte(self) -> bytes:
+    def __read_bytes(self) -> bytes:
         return self.file.read(1)
+
+    def __read_byte(self) -> int:
+        return self.file.read(1)[0]
+
+    def __read_uint16_big_endian(self) -> int:
+        return self.__read_byte() << 8 | self.__read_byte()
 
     def __is_marker_code(self, marker_code: int) -> bool:
         # To prevent marker codes in the encoded bit stream encoders must encode the next byte zero or
@@ -46,19 +53,42 @@ class JpegReader:
 
     def __dump_marker_code(self, marker_code: int) -> None:
         dump_function = self.dump_functions.get(marker_code, None)
-        if dump_function == None:
+        if dump_function is None:
             self.__dump_unknown_marker(marker_code)
         else:
             dump_function()
 
     def __dump_start_of_image_marker(self) -> None:
-        print(f"{self.__get_start_offset():8} Marker 0xFFD8: SOI (Start Of Image), defined in ITU T.81/IEC 10918-1")
+        print(f"{self.__get_start_offset :8} Marker 0xFFD8: SOI (Start Of Image), defined in ITU T.81/IEC 10918-1")
+
+    def __dump_start_of_frame_jpegls(self) -> None:
+        print(f"{self.__get_start_offset :8} Marker 0xFFF7: SOF_55 (Start Of Frame JPEG-LS),"
+              f" defined in ITU T.87/IEC 14495-1 JPEG LS")
+        print(f"{self.__position:8}  Size = {self.__read_uint16_big_endian()}")
+        print(f"{self.__position:8}  Sample precision (P) = {self.__read_byte()}")
+        print(f"{self.__position:8}  Number of lines (Y) = {self.__read_uint16_big_endian()}")
+        print(f"{self.__position:8}  Number of samples per line (X) = {self.__read_uint16_big_endian()}")
+        position: int = self.__position
+        component_count: int = self.__read_byte()
+        print(f"{position:8}  Number of image components in a frame (Nf) = {component_count}",)
+        for component in range(component_count):
+            print(f"{self.__position:8}   Component identifier (Ci) = {self.__read_byte()}")
+            position = self.__position
+            sampling_factor: int = self.__read_byte()
+            print(f"{position:8}   H and V sampling factor (Hi + Vi) = {sampling_factor}"
+                  f" ({sampling_factor >> 4} + {sampling_factor & 0xF})") 
+            print(f"{self.__position:8}   Quantization table (Tqi) [reserved, should be 0] = {self.__read_byte()}")
 
     def __dump_unknown_marker(self, marker_code: int) -> None:
-        print(f"{self.__get_start_offset():8} Marker 0xFF{marker_code:02X}")
+        print(f"{self.__get_start_offset :8} Marker 0xFF{marker_code:02X}")
 
+    @property
+    def __position(self) -> int:
+        return self.file.tell()
+
+    @property
     def __get_start_offset(self) -> int:
-        return self.file.tell() - 2
+        return self.__position - 2
 
 
 def main() -> int:
